1e46903bb0e39d369a8b257595b2ec1291bcbf1a
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-11-20 14:27:29 +0200

    1

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import './assets/main.css';
 import 'preline/dist/preline'; // Import the precompiled version
 import VueCookies from 'vue-cookies'; // Import vue-cookies
+import router from './router'; // Import the router instance
 
 const app = createApp(App);
 
@@ -11,5 +12,8 @@ app.use(VueCookies);
 
 // Optional: Set a global default expiration time for cookies (e.g., 7 days)
 app.config.globalProperties.$cookies.config('7d');
+
+// Use the router
+app.use(router);
 
 app.mount('#app');
