0b172c059c4757a7d1bd357fdda2a558c1824029
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-11-20 13:08:25 +0200

    1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/main.css';
-import 'preline';
+import 'preline/dist/preline'; // Import the precompiled version
+
 
 createApp(App).mount('#app')
