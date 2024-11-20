bc140e20e9f82e3461fee821a3565630f37a7e22
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-11-20 16:24:58 +0200

    1

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,15 +11,20 @@ module.exports = {
     parser: 'vue-eslint-parser', // Ensure the correct parser is set
     parserOptions: {
         parser: '@babel/eslint-parser', // Use Babel parser for `<script>` section
-        requireConfigFile: false,       // Prevent need for Babel config
-        ecmaVersion: 2020,              // Enable modern JavaScript syntax
+        requireConfigFile: false, // Prevent need for Babel config
+        ecmaVersion: 2020, // Enable modern JavaScript syntax
         sourceType: 'module',
     },
     rules: {
         'vue/no-multiple-template-root': 'off', // Allow multiple root nodes in Vue 3
-        'no-unused-vars': 'warn',
-        'vue/no-unused-components': 'off', // Disable "unused components" errors
+        'no-unused-vars': 'warn', // Warn about unused vars
+        'no-empty': 'off', // Disable no-empty rule
+        'no-console': 'off', // Disable no-console rule
+        'no-debugger': 'off', // Disable no-debugger rule
+        'no-empty-function': 'off', // Disable no-empty-function rule
+        'no-dupe-keys': 'off', // Disable duplicate keys in object literals
+        'vue/no-unused-components': 'off', // Disable unused components rule
         'vue/valid-v-slot': 'off', // Disable the 'valid-v-slot' error
-        'no-dupe-keys': 'off', // Disable the 'duplicate key' error in Vue component definitions
+        'vue/require-default-prop': 'off', // Disable the rule for requiring default value type to be a function
     },
 };
