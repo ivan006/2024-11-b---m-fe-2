ae74e839ba0bc135a56ea52881e8f1a4e879af5a
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-11-20 14:17:10 +0200

    1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,20 @@
 module.exports = {
-    extends: ['plugin:vue/vue3-recommended'],
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/vue3-recommended', // Use Vue 3 recommendations
+        'eslint:recommended',
+    ],
+    parser: '@babel/eslint-parser', // Use Babel parser
+    parserOptions: {
+        requireConfigFile: false, // Allows Babel parser to work without a separate Babel config file
+        ecmaVersion: 2020,       // Support modern JavaScript
+        sourceType: 'module',
+    },
     rules: {
-        'vue/no-undef': 'off', // Disable error for defineOptions
-        'no-unused-vars': 'warn', // Warn instead of error for unused variables
+        'no-unused-vars': 'warn',
+        'no-undef': 'off', // Disable undefined errors for `static` fields
     },
 };
